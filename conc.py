403203532079dc5f0152d35a39bb9b2eaa3c6c37
FGM75 <fgonzalezm75@gmail.com>
2023-01-20 16:21:53 +0100

    se añaden comentarios

--- conc.py
+++ conc.py
@@ -2,6 +2,9 @@ a = 33
 b = 64
 x = a + b
 print(x)
+'''se imprime las variables sumados como int'''
 x = str(x)
+'''aqui se transforma en string para cambiar el tipo de variable'''
 print(type(x))
+'''se imprime el nuevo resultado'''
 print(x)
